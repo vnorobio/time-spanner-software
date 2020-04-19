67468dd924db4392a0fdb624e61fb14d3713be08
Victor Orobio <vnorobio@gmail.com>
2020-04-19 18:37:51 -0500

    Project initialization

---
+++ src/main/java/com/neytor/timespannersoftware/TimeSpannerApplication.java
@@ -0,0 +1,13 @@
+package com.neytor.timespannersoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSpannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSpannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neytor/timespannersoftware/TimeSpannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neytor.timespannersoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSpannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
